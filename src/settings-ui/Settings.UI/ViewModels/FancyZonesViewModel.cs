5ccd5cee48b7e67c12082abe9c322d50fb6fedea
quyenvsp <quyenvsp@gmail.com>
2024-12-28 11:21:40 +0700

    feat(FancyZones): add setting always maximize for moved to the current active monitor

--- src/settings-ui/Settings.UI/ViewModels/FancyZonesViewModel.cs
+++ src/settings-ui/Settings.UI/ViewModels/FancyZonesViewModel.cs
@@ -87,6 +87,7 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
             _allowChildWindowSnap = Settings.Properties.FancyzonesAllowChildWindowSnap.Value;
             _disableRoundCornersOnSnap = Settings.Properties.FancyzonesDisableRoundCornersOnSnap.Value;
             _highlightOpacity = Settings.Properties.FancyzonesHighlightOpacity.Value;
+            _alwaysMaximize = Settings.Properties.FancyzonesALwaysMaximize.Value;
             _excludedApps = Settings.Properties.FancyzonesExcludedApps.Value;
             _systemTheme = Settings.Properties.FancyzonesSystemTheme.Value;
             _showZoneNumber = Settings.Properties.FancyzonesShowZoneNumber.Value;
@@ -175,6 +176,7 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
         private bool _disableRoundCornersOnSnap;
 
         private int _highlightOpacity;
+        private string _alwaysMaximize;
         private string _excludedApps;
         private HotkeySettings _editorHotkey;
         private bool _windowSwitching;
@@ -858,6 +860,24 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                     }
 
                     Settings.Properties.FancyzonesPrevTabHotkey.Value = _prevTabHotkey;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string AlwaysMaximize
+        {
+            get
+            {
+                return _alwaysMaximize;
+            }
+
+            set
+            {
+                if (value != _alwaysMaximize)
+                {
+                    _alwaysMaximize = value;
+                    Settings.Properties.FancyzonesALwaysMaximize.Value = value;
                     NotifyPropertyChanged();
                 }
             }
